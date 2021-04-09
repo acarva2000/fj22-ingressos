8778e6590aab5a1ad8a33a91378532a497a037a0
Anderson Carvalho <acarva2000@gmail.com>
2021-04-09 11:11:29 -0300

    Implementando compra

--- src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
+++ src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
@@ -18,6 +18,7 @@ import org.springframework.web.servlet.ModelAndView;
 import br.com.caelum.ingresso.dao.FilmeDao;
 import br.com.caelum.ingresso.dao.SalaDao;
 import br.com.caelum.ingresso.dao.SessaoDao;
+import br.com.caelum.ingresso.model.Carrinho;
 import br.com.caelum.ingresso.model.ImagemCapa;
 import br.com.caelum.ingresso.model.Sessao;
 import br.com.caelum.ingresso.model.TipoDeIngresso;
@@ -32,10 +33,13 @@ public class SessaoController {
 	private SessaoDao sessaoDao;
 
 	@Autowired
+	private Carrinho carrinho;
+
+	@Autowired
 	private SalaDao salaDao;
 	@Autowired
 	private FilmeDao filmeDao;
-	
+
 	@Autowired
 	private OmdbClient client;
 
@@ -66,14 +70,15 @@ public class SessaoController {
 	@GetMapping("/sessao/{id}/lugares")
 	public ModelAndView lugaresNaSessao(@PathVariable("id") Integer sessaoId) {
 		ModelAndView modelAndView = new ModelAndView("sessao/lugares");
-		
+
 		Sessao sessao = sessaoDao.findOne(sessaoId);
-		
+
 		Optional<ImagemCapa> imagemCapa = client.request(sessao.getFilme(), ImagemCapa.class);
-		
+
 		modelAndView.addObject("sessao", sessao);
+		modelAndView.addObject("carrinho", carrinho);
 		modelAndView.addObject("imagemCapa", imagemCapa.orElse(new ImagemCapa()));
-		modelAndView.addObject("tiposDeIngressos",	TipoDeIngresso.values());
+		modelAndView.addObject("tiposDeIngressos", TipoDeIngresso.values());
 
 		return modelAndView;
 	}
